28c4bed0dad292792452107ba9253c5241767ff5
WellitonGalantt <devcode.welliton@gmail.com>
2025-10-28 06:34:22 -0300

    Logs for test

--- src/modules/leads/leads.integration.ts
+++ src/modules/leads/leads.integration.ts
@@ -8,6 +8,8 @@ export async function leadIntegrationN8n(date: ILead): Promise<void> {
   try {
     const webhookUrl = process.env.WEBHOOK_URL ?? "";
     // WEBHOOK do n8n que automatiza mensagens e planilha
+    console.log("Webhook URL:", process.env.WEBHOOK_URL);
+    console.log(webhookUrl)
     const res = await fetch(webhookUrl, {
       method: "POST",
       body: JSON.stringify(date),
@@ -19,6 +21,7 @@ export async function leadIntegrationN8n(date: ILead): Promise<void> {
     //     "message": "Esse email ja esta registrado!"
     // }
     const json = await res.json();
+    console.log("Result Fetch Back", json);
 
     // Se for false eu gero uma excessao para tratar no controller
     if (!json.sucess) {
